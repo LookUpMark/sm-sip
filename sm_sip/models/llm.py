e42765baa2cc823b74bfc893c6f491f0a131e62c
Marc'Antonio Lopez <marcantoniolopez0@gmail.com>
2026-02-16 22:26:13 +0100

    fix: revert llm_int8_enable_fp32_cpu_offload (bitsandbytes SCB bug)

--- sm_sip/models/llm.py
+++ sm_sip/models/llm.py
@@ -43,10 +43,7 @@ def load_llm(
     if quantization == "4bit":
         bnb_config = BitsAndBytesConfig(load_in_4bit=True)
     else:
-        bnb_config = BitsAndBytesConfig(
-            load_in_8bit=True,
-            llm_int8_enable_fp32_cpu_offload=True,
-        )
+        bnb_config = BitsAndBytesConfig(load_in_8bit=True)
 
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
